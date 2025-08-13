b0d7a8387d733766fb373837b91839117b1820e2
unknown <mahammedrabbani8229@gmail.com>
2025-08-13 17:32:07 +0530

    first commit

---
+++ src/main/java/com/kafka/kafkacommon/dto/OrderEvent.java
@@ -0,0 +1,14 @@
+package com.kafka.kafkacommon.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+    private String message;
+    private String Status;
+    private Order order;
+}
